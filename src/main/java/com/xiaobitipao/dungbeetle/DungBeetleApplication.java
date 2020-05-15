dc842390d52ab538cda41a68b8e88be8e250626d
lzzheng <lzzheng@cn.ibm.com>
2020-05-15 10:28:49 +0800

    Initial SpringBoot Initializr

---
+++ src/main/java/com/xiaobitipao/dungbeetle/DungBeetleApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobitipao.dungbeetle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungBeetleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DungBeetleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaobitipao/dungbeetle/DungBeetleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobitipao.dungbeetle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungBeetleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
